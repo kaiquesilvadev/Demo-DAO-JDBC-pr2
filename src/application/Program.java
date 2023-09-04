44169ea427a39b2621e3d63e880873d1ed57da16
kaiquesilvadev <j.kaique1570@gmail.com>
2023-09-04 20:47:37 -0300

    teste findByid

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,10 +1,24 @@
 package application;
 
+import java.util.Scanner;
+
+import model.dao.DaoFactory;
+import model.dao.SellerDao;
+import model.entities.Seller;
+
 public class Program {
 
 	public static void main(String[] args) {
 	
-
+        Scanner sc = new Scanner(System.in);
+		
+		SellerDao sellerDao = DaoFactory.createSellerDao();
+		
+		System.out.println("=== TEST 1: seller findById =====");
+		Seller seller = sellerDao.findById(3);
+		System.out.println(seller);
+		
+		sc.close();
 	}
 
 }
